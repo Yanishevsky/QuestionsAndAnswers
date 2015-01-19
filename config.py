04526b05f2af80c126bd48f3a87eb99856e85ee1
Yanishevsky <yanishevskij@gmail.com>
2015-01-19 18:57:03 +0200

    test

--- config.py
+++ config.py
@@ -9,7 +9,11 @@ OPENID_PROVIDERS = [
     { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
     { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
     
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+if os.environ.get('DATABASE_URL') is None:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 # pagination
