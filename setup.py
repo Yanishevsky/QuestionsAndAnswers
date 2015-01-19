4af22840d88e25c8e60fce0a289da4c4b609925a
Yanishevsky <yanishevskij@gmail.com>
2015-01-19 20:06:13 +0200

    test3

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 import os, subprocess, sys
-subprocess.call(['python', 'flask'])
+subprocess.call(['python', 'virtualenv.py', 'flask'])
 if sys.platform == 'win32':
     bin = 'Scripts'
 else:
